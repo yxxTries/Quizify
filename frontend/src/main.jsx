cc4c1188a0c920bbef146c98105c2ead1ded4477
yxxTries <amil.shahul777@gmail.com#>
2026-04-24 05:23:49 -0300

    fixes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
